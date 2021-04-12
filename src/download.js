db3a1d9f8ce7bb0b410c588aaced98b387aa8e1d
PP <PP@DESKTOP-814G3OK>
2021-04-12 16:28:47 +0700

    start download

--- src/download.js
+++ src/download.js
@@ -1,22 +1,73 @@
 
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class download extends Component {
-    render() {
-        return (
-            <div>
-                <table id="jackpotList">
-                    <tr>
-                        <td>
-
-                        </td>
-                        <td>
-
-                        </td>
-                    </tr>
-                </table>
+function download() {
+    return (
+        <div className="container text-center">
+            <h1>ดาวน์โหลด Slotxo</h1>
+            <span>
+                ดาวน์โหลด Slotxo สามารถดาวน์โหลดและติดตั้งแอพพลิเคชั่นสำหรับเข้าเล่น Slotxo<br/>
+                ในมือถือได้ทั้งระบบแอนดรอยด์และระบบ iOS (iPhone) รองรับทุกระบบ<br />
+                โดยท่านสามารถทำตามรายการดังต่อไปนี้จากเราได้เลยคะ
+            </span>
+            <br />
+            <img src="img/game/slotxo.png" className="img-fluid m-4"></img>
+            <br />
+            <span>
+                สำหรับวิธีการดาวน์โหลดนั้น เราจะแยกออกเป็นสองรายการแอพพลิเคชั่นนะคะ นั่นคือ<br />
+                ระบบแอนดรอยด์สำหรับท่านที่ใช้โทรศัพท์มือถือ เช่น ซัมซุง ออปโป้ เป็นต้นคะ<br />
+                ส่วนอีกค่ายหนึ่งก็คือฝั่งของ แอปเปิ้ล หรือว่า iPhone โดยให้ยิง QR Code<br />
+                ชุดด้านล่างนี้ด้วยโทรศัพท์มือถือของท่าน ได้เลยนะคะ
+            </span>
+            <div className="row mt-5 mb-5">
+                <div className="col-6 text-left">
+                    <img src="img/download/WebQRCodeAD.jpg" className="img-fluid"></img>
+                    <h5>ระบบ : ANDROID</h5>
+                    <a-nav className="nav-link" href="#">Download For Android Here</a-nav>
+                    <p>
+                        ระบบเกมส์ถูกพัฒนาโดยทีมงานชาวต่างประเทศที่มีความน่าเชื่อถือและมีความเชี่ยวชาญ
+                        ในระบบของแอนดรอยด์โดยเฉพาะ ท่านจึงมั่นใจได้ในเรื่องของความปลอดภัยของระบบ
+                    </p>
+                    <p>
+                        ทั้งในการเล่นผ่านเว็บบราวเซอร์ต่างๆ หรือจะเป็นการเล่นผ่านแอพพลิเคชั่น
+                        ท่านสามารถสอบถามข้อมูลที่ท่านต้องการได้จากเราตลอด 24 ชั่วโมง นอกจากนี้
+                        ท่านยังสามารถทำรายการต่างๆได้จากช่องทางเดียวกันนี้ได้ทุกรายการ
+                        ไม่ว่าจะเป็นรายการฝากเติม แจ้งถอน สะดวก สบายใจ ความมั่นคงสูง
+                    </p>
+                </div>
+                <div className="col-6 text-left">
+                    <img src="img/download/WebQRCodeAD.jpg" className="img-fluid"></img>
+                    <h5>ระบบ : iOS</h5>
+                    <a-nav className="nav-link" href="#">Download For iOS Here</a-nav>
+                    <p>
+                        ระบบเกมส์ถูกพัฒนาโดยทีมงานชาวต่างประเทศที่มีความน่าเชื่อถือและมีความเชี่ยวชาญ
+                        ในระบบของแอนดรอยด์โดยเฉพาะ ท่านจึงมั่นใจได้ในเรื่องของความปลอดภัยของระบบ
+                    </p>
+                    <p>
+                        ทั้งในการเล่นผ่านเว็บบราวเซอร์ต่างๆ หรือจะเป็นการเล่นผ่านแอพพลิเคชั่น
+                        ท่านสามารถสอบถามข้อมูลที่ท่านต้องการได้จากเราตลอด 24 ชั่วโมง นอกจากนี้
+                        ท่านยังสามารถทำรายการต่างๆได้จากช่องทางเดียวกันนี้ได้ทุกรายการ
+                        ไม่ว่าจะเป็นรายการฝากเติม แจ้งถอน สะดวก สบายใจ ความมั่นคงสูง
+                    </p>
+                </div>
             </div>
-        )
-    }
+
+            <div className="border-danger p-3">
+                <ul className="text-left">
+                    <li><i className="fa fa-check-circle-o" aria-hidden="true"></i>  สมัครง่ายๆ แค่คลิก ระบบเราทำงานอัติโนมัติภายใน 1 นาทีเท่านั้น</li>
+                    <li><i className="fa fa-check-circle-o" aria-hidden="true"></i>  ฝาก-ถอน ระบบอัติโนมัติ AUTO 1 นาที ด้วยตัวคุณเอง</li>
+                    <li><i className="fa fa-check-circle-o" aria-hidden="true"></i>  รองรับเมนูภาษาไทย เล่นพนันผ่านมือถือได้</li>
+                    <li><i className="fa fa-check-circle-o" aria-hidden="true"></i>  สำนักงานที่เป็นรูปธรรมจริงๆ มีช่องทาง การติดต่อมากมาย </li>
+                    <li><i className="fa fa-check-circle-o" aria-hidden="true"></i>  ฝาก-ถอนผ่านหน้าเว็บ อัตโนมัติ 1 นาทีเท่านั้น</li>
+                    <li><i className="fa fa-check-circle-o" aria-hidden="true"></i>  Call Center ตลอด 24 ชั่วโมง รองรับการสอบถาม ปรึกษาและสมัครได้ตลอดเวลา</li>
+                    <li><i className="fa fa-check-circle-o" aria-hidden="true"></i>  ซื่อสัตย์ เชื่อถือได้ การันตี การเงินมั่นคง 100%</li>
+                    <li><i className="fa fa-check-circle-o" aria-hidden="true"></i>  มี เกมส์มากมาย และอื่นๆ แบบสดๆ ให้เล่นได้ในไอดีเดียวกัน</li>
+                    <li><i className="fa fa-check-circle-o" aria-hidden="true"></i>  การเงินมั่นคง จ่ายเร็ว จ่ายไว จ่ายไม่อั้น</li>
+                    <li><i className="fa fa-check-circle-o" aria-hidden="true"></i>  เล่นเกมส์ SLOT พร้อมโปรโมชั่นดีอีกมากมาย</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
+export default download;
