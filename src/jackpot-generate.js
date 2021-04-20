5cad765f2dcad323cfd3db33a910f8d6afb6bc9d
dev01tcm <78786837+dev01tcm@users.noreply.github.com>
2021-04-20 17:44:16 +0700

    start gen json

--- src/jackpot-generate.js
+++ src/jackpot-generate.js
@@ -1,30 +1,50 @@
 import React from "react";
 
 export default function GenerateJackpot() {
-    var arr_hh = [];
+  var data = [];
+
   const generate = () => {
     for (let i = 0; i < 5; i++) {
-        var r1 = Math.floor(Math.random() * 24);
-        var r2 = Math.floor(Math.random() * 60);
-        r1 = ("0" + r1).slice(-2)
-        r2 = ("0" + r2).slice(-2)
-        let testdate = new Date()//
-		let test2 = testdate.setDate(testdate.getDate() - 1);
-		let date = new Intl.DateTimeFormat('en-GB').format(test2);
-		let run = (Math.floor(Math.random() * 29) + 1);
-		let id = '2021'+("000" + run).slice(-3)+'XXX';
-		let formatter = new Intl.NumberFormat('th-TH', {  style: 'currency',  currency: 'THB',});
-		let img = 'img' + (Math.floor(Math.random() * 10) + 1);
-		let test = formatter.format((Math.floor(Math.random() * 1000) + 1) * 10)
+      //---user_id
+      let running = Math.floor(Math.random() * 29) + 1;
+      let user_id = "2021" + ("000" + running).slice(-3) + "XXX";
 
-        document.getElementById.innerHtml = (id + '___' + date + ' ' + r1 + ':' + r2 + '___' + test + '___' + img + "<br>") 
-        if (arr_hh.indexOf(r1) === -1) arr_hh.push(r1);
+      //--date
+      let today = new Date(); //
+      let lastday = today.setDate(today.getDate() - 1);
+      let showdate = new Intl.DateTimeFormat("en-GB").format(lastday);
+
+      //--time
+      let hh = Math.floor(Math.random() * 24);
+      let mm = Math.floor(Math.random() * 60);
+      hh = ("0" + hh).slice(-2);
+      mm = ("0" + mm).slice(-2);
+      let time = hh + ':' + mm
+
+      //--withdraw
+      let formatter = new Intl.NumberFormat("th-TH", {
+        style: "currency",
+        currency: "THB",
+      });
+      let withdraw = formatter.format(
+        (Math.floor(Math.random() * 1000) + 1) * 10
+      );
+
+      //--game img
+      let img = "img" + (Math.floor(Math.random() * 10) + 1);
+
+      data = {user_id:user_id, showdate:showdate + ' ' + time, withdraw:withdraw, img:img}
+      const jsontext =  JSON.stringify(data)
+        console.table(jsontext)
     }
   };
+
+  //---------------------------//
   return (
-  <div>
-    <div className="btn" onClick={generate}>click</div>
-    <p id="result"></p>
-  </div>
+    <div className="content-wrapper">
+      <div className="btn" onClick={generate}>
+        click
+      </div>
+    </div>
   );
 }
