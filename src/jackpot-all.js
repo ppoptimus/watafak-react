f2c720a13ba4331901f080a60ee143c00cd791e9
ppoptimus <r.pumpo@gmail.com>
2021-07-24 22:07:50 +0700

    pagination

--- src/jackpot-all.js
+++ src/jackpot-all.js
@@ -1,54 +1,88 @@
-import React, { useState, useEffect } from "react"
-import Firebase from "./utils/firebase"
+import React, { useState, useEffect } from "react";
+import Firebase from "./utils/firebase";
+import ReactPaginate from "react-paginate";
 
 function AllJackpot() {
-	const [jackpotList, setJackpotList] = useState("")
+  const [jackpotList, setJackpotList] = useState("");
 
-	useEffect(() => {
-		const jackpotRef = Firebase.database().ref("Jackpot")
-		
-		jackpotRef.on("value", (snapshot) => {
-			const data = snapshot.val()
-			const jackpotList = []
-			for (let id in data) {
-				jackpotList.push({ id, ...data[id] })
-			}
-			setJackpotList(jackpotList)
-			
-		})
-	}, [])
+  useEffect(() => {
+    const jackpotRef = Firebase.database().ref("Jackpot");
 
-	return (
-		<div className='container mb-6 mt-5'>
-			{jackpotList
-				? jackpotList
-						.sort((a,b) => (a.showdate < b.showdate ? 1 : -1))
-						.map((i) => (
-							<div className='jackpot-card' key={i.id}>
-								<div className='row col-12 pr-0 pl-0 middle'>
-									<div className='col-5 text-center pr-0 pl-0'>
-										<p className='mb-0'>{i.user_id}</p>
-										<p className='mb-0 text-point'>
-											{i.showdate} 
-										</p>
-									</div>
-									<div className='col-4'>
-										<p className='mb-0'>แจ้งถอน</p>
-										<p className='mb-0 text-point'>
-											{i.withdraw} <img src='img/coin.png' className='img-fluid' alt='watafak88 coin' />
-										</p>
-									</div>
-									<div className='col-3 pr-1 pl-0'>
-										<div className='full-page-loader'>
-											<img className='img-fluid w-50' src={"img/download/" + i.img + ".png"} alt='watafak88 game jackpot' imagesizes='50vw'></img>
-										</div>
-									</div>
-								</div>
-							</div>
-						))
-				: "Loading.."}
-		</div>
-	)
+    jackpotRef.on("value", (snapshot) => {
+      const data = snapshot.val();
+      const jackpotList = [];
+      for (let id in data) {
+        jackpotList.push({ id, ...data[id] });
+      }
+      jackpotList.sort((a, b) => (a.showdate < b.showdate ? 1 : -1));
+
+      setJackpotList(jackpotList);
+    });
+  }, []);
+
+  const [pageNumber, setPageNumber] = useState(0);
+  const usersPerPage = 20;
+  const pagesVisited = pageNumber * usersPerPage;
+
+  const pageCount = Math.ceil(jackpotList.length / usersPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  return (
+    <div className="container mb-6 mt-5">
+      {jackpotList
+        ? jackpotList
+            .slice(pagesVisited, pagesVisited + usersPerPage)
+            .map((i) => (
+              <div className="jackpot-card" key={i.id}>
+                <div className="row col-12 pr-0 pl-0 middle">
+                  <div className="col-5 text-center pr-0 pl-0">
+                    <p className="mb-0">{i.user_id}</p>
+                    <p className="mb-0 text-point">{i.showdate}</p>
+                  </div>
+                  <div className="col-4">
+                    <p className="mb-0">แจ้งถอน</p>
+                    <p className="mb-0 text-point">
+                      {i.withdraw}{" "}
+                      <img
+                        src="img/coin.png"
+                        className="img-fluid"
+                        alt="watafak88 coin"
+                      />
+                    </p>
+                  </div>
+                  <div className="col-3 pr-1 pl-0">
+                    <div className="full-page-loader">
+                      <img
+                        className="img-fluid w-50"
+                        src={"img/download/" + i.img + ".png"}
+                        alt="watafak88 game jackpot"
+                        imagesizes="50vw"
+                      ></img>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+        : "Loading.."}
+
+      <div className="container mt-4" style={{ textAlign: '-webkit-center' }}>
+        <ReactPaginate
+		style={{alignItems: 'center'}}
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={"paginationBttns"}
+          previousLinkClassName={"btn-nav"}
+          nextLinkClassName={"btn-nav"}
+        //   disabledClassName={"paginationDisabled"}
+          activeClassName={"paginationActive"}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default AllJackpot
+export default AllJackpot;
